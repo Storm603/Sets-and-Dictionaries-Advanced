d665af6fde87ec9200a5f0e6d69b531a204300cc
Storm603 <93555141+Storm603@users.noreply.github.com>
2022-01-24 22:14:47 +0200

    Add files via upload

---
+++ T09. SoftUni Exam Results/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Resources;
+
+namespace T09._SoftUni_Exam_Results
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<string, Dictionary<string, int>> studentCollection = new Dictionary<string, Dictionary<string, int>>();
+            Dictionary<string, int> submissions = new Dictionary<string, int>();
+
+            string input = Console.ReadLine();
+
+            while (input != "exam finished")
+            {
+                string[] split = input.Split("-");
+
+                string name = split[0];
+
+                if (split[1] == "banned") // student ban
+                {
+                    studentCollection.Remove(name);
+                    input = Console.ReadLine();
+                    continue;
+                }
+
+                string language = split[1];
+                int points = int.Parse(split[2]);
+
+                if (!submissions.ContainsKey(language)) // submission counts in 2nd dict
+                {
+                    submissions.Add(language, 0);
+                }
+                submissions[language]++;
+
+                if (!studentCollection.ContainsKey(name)) // checks if student exists in first dict
+                {
+                    studentCollection.Add(name, new Dictionary<string, int>());
+                }
+
+                if (!studentCollection[name].ContainsKey(language)) // checks if student has the language in 1st dict
+                {
+                    studentCollection[name].Add(language, 0);
+                }
+
+                if (studentCollection[name][language] < points) // checks if points are higher than current ones in 1st dict
+                {
+                    studentCollection[name][language] = points;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            studentCollection = studentCollection.OrderByDescending(x => x.Value.Values.Max()).ThenBy(x => x.Key)
+                .ToDictionary(x => x.Key, x => x.Value);
+            Console.WriteLine("Results:");
+            foreach (var name in studentCollection)
+            {
+                Console.WriteLine($"{name.Key} | {name.Value.Values.First()}");
+            }
+
+            submissions = submissions.OrderByDescending(x => x.Value).ThenBy(x => x.Key).ToDictionary(x => x.Key, x => x.Value);
+
+            Console.WriteLine("Submissions:");
+            foreach (var langage in submissions)
+            {
+                Console.WriteLine($"{langage.Key} - {langage.Value}");
+            }
+        }
+    }
+}
---
+++ T10. ForceBook/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T10._ForceBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<string, List<string>> sides = new Dictionary<string, List<string>>();
+
+            string input = Console.ReadLine();
+            string[] split = null;
+            while (input != "Lumpawaroo")
+            {
+                split = input.Split();
+
+                bool isExist = false;
+                if (input.Contains("|"))
+                { 
+                    split = input.Split(" | ");
+
+                    string side = split[0];
+                    string user = split[1];
+
+
+                    if (!sides.ContainsKey(side))
+                    {
+                        sides.Add(side, new List<string>());
+                    }
+
+                    foreach (var side1 in sides)
+                    {
+                        foreach (string user1 in side1.Value)
+                        {
+                            if (user1 == user)
+                            {
+                                isExist = true;
+                                break;
+                            }
+                        }
+                    }
+
+                    if (isExist == false)
+                    {
+                        sides[side].Add(user);
+                    }
+                }
+                else if (input.Contains("->"))
+                {
+                    split = input.Split(" -> ");
+                    string user = split[0];
+                    string side = split[1];
+
+                    foreach (var side1 in sides)
+                    {
+                        foreach (string user1 in side1.Value)
+                        {
+                            if (user1 == user)
+                            {
+                                isExist = true;
+                                sides[side1.Key].Remove(user);
+                                if (side1.Value.Count == 0)
+                                {
+                                    sides.Remove(side1.Key);
+                                }
+                                break;
+                            }
+                        }
+                    }
+
+                    if (isExist == true)
+                    {
+                        sides[side].Add(user);
+                        Console.WriteLine($"{user} joins the {side} side!");
+                    }
+                    else
+                    {
+                        sides[side].Add(user);
+                        Console.WriteLine($"{user} joins the {side} side!");
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+
+            //sides = sides.OrderByDescending(x => x.Value.Count).ToDictionary(x => x.Key, x => x.Value);
+
+            foreach (var side in sides.OrderByDescending(x => x.Value.Count))
+            {
+                Console.WriteLine($"Side: {side.Key}, Members: {side.Value.Count}");
+                foreach (var name in side.Value.OrderBy(x => x))
+                {
+                    Console.WriteLine($"! {name}");
+                }
+            }
+        }
+    }
+}
